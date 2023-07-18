f5988a643783966968767e3322a8ed726ddbfad4
Rohith Gowda M <rohithgowda150503@gmail.com>
2023-07-18 23:41:32 +0530

    edit1

--- list/urls.py
+++ list/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.list,name='list'),
-    path('homepage',views.homepage,name='homepage'),
-    path('student',views.student,name='student'),
+    path('',views.test,name='test'),
+
 ]
--- list/views.py
+++ list/views.py
@@ -5,29 +5,7 @@ from django.contrib.auth import authenticate,login
 from django.contrib.auth import logout
 
 # Create your views here.
-def list(request):
-    return HttpResponse("Hello world")
+def test(request):
+    while(1):
+        return render(request,"home.html")
 
-def homepage(request):  
-  return render(request,'homepage.html')
-
-def loginuser(request):
-    if request.method=="POST":
-        username=request.POST.get('username')
-        password=request.POST.get('password')
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            # A backend authenticated the credentials
-            login(request,user)
-            return redirect("/")
-        else:
-            # No backend authenticated the credentials
-            return render(request,'homepage.html')
-    return render(request,'homepage.html')
-  
-def logoutUser(request):
-  logout(request)
-  return redirect("/homepage")
-
-def student(request):
-    return render(request,'student.html')
